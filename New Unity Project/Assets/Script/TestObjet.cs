81bc40387f707b710d8af4f2ee9259273d201370
OROCHI <tungus_v@yahoo.co.jp>
2015-06-18 10:27:17 +0900

    テスト用プロジェクト

---
+++ New Unity Project/Assets/Script/TestObjet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestObjet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("TestProject Done!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
